40f9000ed5d6cbfadbe7e312f1adb14e3ea3e78b
Jeronimo Sola <cuchujero@hotmail.com>
2022-03-17 13:53:50 -0300

    fix logs

--- public/js/processData.js
+++ public/js/processData.js
@@ -3,10 +3,9 @@ let selectCompanies = document.getElementById('companie');
 fetch('http://localhost:3000/enviromentUrlCompanies')
     .then(r => r.json())
     .then(q => {
-        fetch("https://testpackage123.herokuapp.com/companies")
+        fetch(q.url)
             .then(res => res.json())
             .then(res => {
-                console.log("v",res)
                 for (e of res) {
                     var opt = document.createElement("option");
                     opt.value = e.id;
